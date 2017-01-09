eaa3e7d092003bbf0bd84dc9dc875639c9863bef
Alba Fernández <alba.fu@yahoo.es>
2017-01-09 10:32:21 -0600

    text to spetch

--- index.php
+++ index.php
@@ -15,7 +15,35 @@
 				<h1 id = "message"><?php echo "Hello IBM World!"; ?></h1>
 				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
 			</td>
+			<td>
+				<input id='txtText' type="text"/>
+			</td>
+			<td>
+				
+			</td>
 		</tr>
 	</table>
+	<script>
+		var voice = 'en-US_AllisonVoice';
+	var token = {
+  "url": "https://stream.watsonplatform.net/text-to-speech/api",
+  "password": "ymeWhvDbaGX6",
+  "username": "e1b38282-a8b6-4ec8-82ca-5645f65f0075"
+};
+	var wsURI = 'wss://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?voice=' +
+				  voice + '&watson-token=' + token;
+	var websocket = new WebSocket(wsURI);
+	websocket.onopen = function(evt) { onOpen(evt) };
+	websocket.onclose = function(evt) { onClose(evt) };
+	websocket.onmessage = function(evt) { onMessage(evt) };
+	websocket.onerror = function(evt) { onError(evt) };
+	function onOpen(evt){
+			console.log(evt)
+		}
+		
+		websocket.send(JSON.stringify(message));
+		
+	
+	</script>
 </body>
 </html>
